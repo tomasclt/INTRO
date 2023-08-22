ae3382bf067494dff1867230d1c9075bf676eb82
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-08-22 11:21:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from PIL import Image
 
 st.title(" Mi Primera App!!")
 
+st.subheader("En este espacio comienzo a desarrollar mis aplicaciones ")
+st.write("Facilmente puedo realizar backend y frontend")
 image = Image.open('Interfaces Mult2.png')
 
 st.image(image, caption='Sunrise by the mountains')
