79352134408cec9ea0a56ff28c15f4397ddc9981
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-08-22 11:34:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ st.write("Facilmente puedo realizar backend y frontend.")
 image = Image.open('Interfaces Mult2.png')
 
 st.image(image, caption='Interfaces multimodales')
+
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', title)
