b070b7feea0c186a457bdb8c90856d9376fb967a
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-08-22 13:10:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ else:
     st.write('No has presionado aún')
 
 with st.sidebar:
+    st.subheader("Configura la modalidad")
     mod_radio = st.radio(
         "Escoge la modalidad a usar",
         ("Visual", "Auditiva","Háptica")
