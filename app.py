ff295388ddb0630f81c971b7ebd9428198b8fa91
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-08-22 11:24:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 st.title(" Mi Primera App!!")
 
 st.subheader("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
-st.write("Facilmente puedo realizar backend y frontend")
+st.write("Facilmente puedo realizar backend y frontend.")
 image = Image.open('Interfaces Mult2.png')
 
 st.image(image, caption='Sunrise by the mountains')
