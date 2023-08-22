38da661402c0343035909398da81e1813139116b
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-08-22 11:40:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,23 @@ st.image(image, caption='Interfaces multimodales')
 
 texto = st.text_input('Escribe algo', 'Este es mi texto')
 st.write('El texto escrito es', texto)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    agree = st.checkbox('I agree')
+    if agree:
+       st.write('Great!')
+   # st.radio(
+   #     "Set text input label visibility 👉",
+   #     key="visibility",
+   #     options=["visible", "hidden", "collapsed"],
+   # )
+   # st.text_input(
+   #     "Placeholder for the other text input widget",
+   #     "This is a placeholder",
+    #    key="placeholder",
+    #)
+
+with col2:
+    st.write("Esta es la segunda columna")
